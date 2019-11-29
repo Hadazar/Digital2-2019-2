067021f6efae85fc8e5bf4b5a5da257a10c604d3
hadazar <hadazar@unal.edu.co>
2019-11-29 18:23:58 -0500

    Se agrega la función  para capturar el código con el boton start

--- Taller2/Compilador.py
+++ Taller2/Compilador.py
@@ -7,6 +7,7 @@ ventana = Tk() #Crea la ventana
 #Configuración de ventana -------------
 ventana.title("Compilador MIPS")
 ventana.geometry("800x600")
+ventana.resizable(0,0) #Bloquear el tamaño de la ventana
 ventana.configure(background="#B1C340")
 
 #Configuración de botones -------------
@@ -14,18 +15,23 @@ ColorBoton=("#3FC3D8")
 AnchoBoton=11
 AltoBoton=3
 
+Codigo = StringVar()
+
 def getOpcode():
     print("Opcode: \n")
 
+def capturarCodigo():
+    Codigo = CodigoMips.get(1.0,END)
+    print("Codigo: ", Codigo)
+
 #Botones -----------------------------
-Iniciar = Boton0=Button(ventana,text="Iniciar",bg=ColorBoton,width=AnchoBoton,height=AltoBoton,command=getOpcode).place(x=17,y=17)
+Iniciar = Boton0=Button(ventana,text="Iniciar",bg=ColorBoton,width=AnchoBoton,height=AltoBoton,command=lambda:capturarCodigo()).place(x=17,y=17)
 Exportar = Boton0=Button(ventana,text="Exportar",bg=ColorBoton,width=AnchoBoton,height=AltoBoton,command=getOpcode).place(x=17+115+17,y=17)
 
 #Código MIPS ---------------------------
 CodigoMips = Text(ventana,width=92,height=15, padx = 10, pady = 10)
 CodigoMips.place(x=17,y=100)
 CodigoMips.insert(END,"Escriba aquí su código Mips...")
-#CodigoMips.pack()
 
 #Estado del programa --------------------
 Estatus = 'Compilando...'
